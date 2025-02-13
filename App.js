c619c4acec8c7c3ab4d516a2936b49e04f964865
Tran Anh Tuan <tuanta@enterprisenao.com>
2025-02-13 17:38:56 +0700

    bai 4: component Button

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const handlePressBtn = () => {
+    alert("bạn đã nhấn vào button");
+  };
+
+  // =================== render ===================
   return (
     <View style={styles.container}>
       <Text>TUANTRAN - REACT - NATIVE - 123 !</Text>
       <StatusBar style="auto" />
+
+      {/* ==== CÚ PHÁP: SAI === */}
+      {/* <Button>Text Button</Button> */}
+
+      {/* ==== CÚ PHÁP: ĐÚNG === */}
+      {/* title: là text hiển thị */}
+      {/* onPress: là xử lý sự kiện nhấn nút - ko phải onClick */}
+      <Button title="Text Button" onPress={handlePressBtn} />
     </View>
   );
 }
