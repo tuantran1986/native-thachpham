f0a4aa8146eb3a9ee954737b6f5d09273de1ea01
Tran Anh Tuan <tuanta@enterprisenao.com>
2025-02-14 17:33:11 +0700

    bai 13: ds phang = FLAT LIST

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import AppBai12 from "./Bai12/AppBai12";
+// import AppBai12 from "./Bai12/AppBai12";
+import Bai13FlatList from "./Bai13FlatList/Bai13FlatList";
 
 export default function App() {
-  return <AppBai12 />;
+  // return <AppBai12 />;
+  return <Bai13FlatList />;
 }
---
+++ Bai13FlatList/Bai13FlatList.js
@@ -0,0 +1,37 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+// =================== COMPONENT - "CHÍNH" ===================
+export default function Bai13FlatList() {
+  // 2 thuộc tính của FlatList = data + renderItem
+  const data = [
+    { id: 1, title: "flat list item - 1" },
+    { id: 2, title: "flat list item - 2" },
+    { id: 3, title: "flat list item - 3" },
+    { id: 4, title: "flat list item - 4" },
+    { id: 5, title: "flat list item - 5" },
+    { id: 6, title: "flat list item - 6" },
+    { id: 7, title: "flat list item - 7" },
+    { id: 8, title: "flat list item - 8" },
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={styleFlatList.itemFlatList}>
+      <Text>{item.title}</Text>
+    </View>
+  );
+
+  // --------- render ---------
+  return <FlatList data={data} renderItem={renderItem} />;
+}
+
+// ============ style ============
+const styleFlatList = StyleSheet.create({
+  itemFlatList: {
+    width: "100%",
+    height: 286,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "green",
+    margin: 10,
+  },
+});
